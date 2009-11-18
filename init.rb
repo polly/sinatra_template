79de3a941bdba19d3bcde30d68a19c6e7ee4c5dd
Patrik Hedman <patrik@moresale.se>
2009-11-18 19:03:28 +0100

    removed rubygems dependency and added configuration for views and public directories

--- init.rb
+++ init.rb
@@ -1,15 +1,17 @@
 APP_ROOT = File.expand_path(File.dirname(__FILE__))
 
-require 'rubygems'
-
 Dir["vendor/sinatra/*/lib/"].each { |dir| $:.unshift(File.join(APP_ROOT, dir)) }
 Dir["vendor/gems/*/lib/"].each do |dir| 
   $:.unshift(File.join(APP_ROOT, dir))
   require Dir[File.join(APP_ROOT, dir, "*.rb")][0]
 end
 
+require 'sinatra'
+
 $:.unshift File.join(APP_ROOT, 'lib')
 $:.unshift File.join(APP_ROOT, 'public')
 
-require 'sinatra'
+set :views,  APP_ROOT + '/views'
+set :public, APP_ROOT + '/public'
+
 require 'main'

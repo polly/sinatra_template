a2f1eca801ed4c62f314658e737dc158b1d378e7
Patrik Hedman <patrik@moresale.se>
2009-11-19 10:31:41 +0100

    updatet test to use webrat

--- test/main_test.rb
+++ test/main_test.rb
@@ -1,20 +1,10 @@
 require File.join(File.dirname(__FILE__), '..', 'test', 'test_helper.rb')
 
 class MainTest < Test::Unit::TestCase
-
-  context "main" do
-    context "GET /" do
-      setup do
-        get '/'
-      end
-      
-      should "be successfull" do
-        assert_equal status, 200
-      end
-
-      should "have a body" do
-        assert body.include? "Hello from sinatra"
-      end
+  context "visit /" do
+    should "display index properly" do
+      visit "/"
+      assert_contain "Hello from sinatra"
     end
   end
 end
